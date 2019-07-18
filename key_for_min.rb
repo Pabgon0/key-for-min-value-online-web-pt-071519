20b3aaac951765059b9bc6f4cbc0bb6a2f14166a
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-18 17:00:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,7 @@
 def key_for_min_value(name_hash)
 i = 0
 key = []
-if name_hash == {}
-  return nil
-end
+return nil if name_hash == {}
   name_hash.each do |name, value|
     if i == 0
       i = value
